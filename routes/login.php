03f5c6a244901ea82680d58728e2e53f8573daac
Brendon de Beer <91954705+monaco-lux@users.noreply.github.com>
2022-01-12 23:44:51 +0200

    Commmit explainer

--- routes/login.php
+++ routes/login.php
@@ -140,5 +140,10 @@ $app->patch('/api/login/patchpass', function (Request $request, Response $respon
     }
 });
 
+/* Note for people reading my commit: I originally created this project
+under the heading "online_store_backend", which is on my github, however
+I managed to mess up the connection and I wasn't able to restore it. The code
+above is from that project. I am merely restoring it here as it now works.
+*/
 
  ?>
