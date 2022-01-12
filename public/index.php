90cff1ebcc94da7263b403bec71d45cbfdb14667
Brendon de Beer <91954705+monaco-lux@users.noreply.github.com>
2022-01-12 23:37:22 +0200

    call database code

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
+// call database
+require __DIR__ . '/../config/db.php';
 
 $app = AppFactory::create();
 
