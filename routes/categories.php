d45dd63d5b66d2e932a25c73f35683ab11a70b79
Brendon de Beer <91954705+monaco-lux@users.noreply.github.com>
2022-01-13 00:00:15 +0200

    categories endpoint

---
+++ routes/categories.php
@@ -0,0 +1,66 @@
+<?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+// code to get all categories
+$app->get('/api/categories/all', function (Request $request, Response $response) {
+    // sql statement to get all products
+    $sql = "SELECT * FROM categories_endpoint";
+
+
+    // connection section with incorporated error code checking
+    try
+    {
+      $db = new DB();
+      $conn = $db->connect();
+
+      $stmt = $conn->query($sql);
+      $product = $stmt->fetchAll(PDO::FETCH_OBJ);
+
+      $db = null;
+      $response->getBody()->write(json_encode($product));
+      return $response
+        ->withHeader('content-type', 'application/json')
+        ->withStatus(200);
+    } catch (PDOException $e)
+    {
+      $error = ["message" => $e->getMessage()];
+      $response->getBody()->write(json_encode($error));
+      return $response
+        ->withHeader('content-type', 'application/json')
+        ->withStatus(500);
+    }
+});
+
+// single entry only
+
+$app->get('/api/categories/{id}', function (Request $request, Response $response, array $args) {
+    $id = $args['id'];
+    $sql = "SELECT * FROM categories_endpoint WHERE id = $id";
+
+    try
+    {
+      $db = new DB();
+      $conn = $db->connect();
+
+      $stmt = $conn->query($sql);
+      $product = $stmt->fetch(PDO::FETCH_OBJ);
+
+      $db = null;
+      $response->getBody()->write(json_encode($product));
+      return $response
+        ->withHeader('content-type', 'application/json')
+        ->withStatus(200);
+    } catch (PDOException $e)
+    {
+      $error = ["message" => $e->getMessage()];
+      $response->getBody()->write(json_encode($error));
+      return $response
+        ->withHeader('content-type', 'application/json')
+        ->withStatus(500);
+    }
+});
+?>
