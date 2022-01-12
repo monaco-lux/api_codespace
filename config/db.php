8719b1a43080f51c2eab2486f76119ebc8b9a3d5
Brendon de Beer <91954705+monaco-lux@users.noreply.github.com>
2022-01-12 23:35:30 +0200

    Code to connect to database (sourced from a previous project)

---
+++ config/db.php
@@ -0,0 +1,21 @@
+<?php
+
+class DB
+{
+  private $host = 'sql11.freemysqlhosting.net';
+  private $user = 'sql11453113';
+  private $pass = 'mwI8ip452n';
+  private $dbname = 'sql11453113';
+
+  public function connect()
+  {
+    $conn_string = "mysql:dbname=$this->dbname;host=$this->host";
+    $conn = new PDO($conn_string,$this->user, $this->pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    return $conn;
+  }
+
+
+}
+?>
